81b989237164db2c6ddeba3d34050767c327345a
Alp <alpcorreacastro@gmail.com>
2020-09-16 11:14:33 +0200

    add settings icon

--- lib/main.dart
+++ lib/main.dart
@@ -76,6 +76,19 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.settings,
+                  size: 26.0,
+                ),
+              )
+          ),
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
