cc7e81a97f0e9bbc0a915e33591b3c19765b33d3
Alp <alpcorreacastro@gmail.com>
2020-10-27 15:24:06 +0100

    added Switchbutton

--- lib/pages/settingsMenu.dart
+++ lib/pages/settingsMenu.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 
-class Settings extends StatelessWidget {
+
+class Settings extends StatefulWidget {
+  Settings({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  SettingsState createState() => SettingsState();
+}
+
+class SettingsState extends State<Settings>{
+
+  bool isSwitched = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: FlatButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Zurück'),
+        appBar: AppBar(
+          title: Text("Einstellungen"),
         ),
-      ),
+
+        body: Center(
+          child: Switch(
+            value: isSwitched,
+            onChanged: (value) {
+              setState(() {
+                isSwitched = value;
+                print(isSwitched);
+              });
+            },
+            activeTrackColor: Colors.lightGreenAccent,
+            activeColor: Colors.green,
+          ),
+        )
+      //child: FlatButton(
+      // onPressed: () {
+      // Navigator.pop(context);
+      // },
+      // child: Text('Zurück'),
+      //),
+      //),
     );
   }
 }
-
-
