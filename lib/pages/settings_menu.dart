a183ecb475583a64456b1e83bed6fe9bc20c49a1
Alp <alpcorreacastro@gmail.com>
2020-11-01 19:10:58 +0100

    change slider to number input field

--- lib/pages/settings_menu.dart
+++ lib/pages/settings_menu.dart
@@ -1,11 +1,12 @@
 import 'package:corona_diary/models/settings.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flutter/services.dart';
+
 import 'privacy_policy_page.dart';
 
 bool isDarkMode;
-double _currentSliderValue;
 
 class SettingsPage extends StatefulWidget {
   SettingsPage({Key key, this.title}) : super(key: key);
@@ -27,33 +28,36 @@ class SettingsPageState extends State<SettingsPage> {
           builder: (BuildContext context, AsyncSnapshot<Settings> snapshot) {
             if (!snapshot.hasData) return CircularProgressIndicator();
             final settings = snapshot.data;
-            _currentSliderValue = settings.daysUntilRemoval.toDouble();
+
             isDarkMode = settings.darkMode;
-            return Column(children: [
-              DarkMode(settings: settings),
-              Text("Tage bis zur Löschung eines Eintrags"),
-              AutomaticDeletion(settings: settings),
-              FlatButton(
-                child: Text("Datenschutzerklärung"),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => PrivacyPolicyPage()));
-                },
-              ),
-              FlatButton(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Text('Lizenzen'),
+            return Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  DarkMode(settings: settings),
+                  Text("Tage bis zur Löschung eines Eintrags"),
+                  AutomaticDeletion(settings: settings),
+                  FlatButton(
+                    child: Text("Datenschutzerklärung"),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PrivacyPolicyPage()));
+                    },
                   ),
-                  onPressed: () {
-                    showAboutDialog(context: context, children: <Widget>[
-                      Text("Made by Alp, Nika, Liadan and Steven with ♥ in"
-                          " Berlin"),
-                    ]);
-                  }),
-            ]);
+                  FlatButton(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text('Lizenzen'),
+                      ),
+                      onPressed: () {
+                        showAboutDialog(context: context, children: <Widget>[
+                          Text("Made by Alp, Nika, Liadan and Steven with ♥ in"
+                              " Berlin"),
+                        ]);
+                      }),
+                ]);
           },
         ));
   }
@@ -108,55 +112,126 @@ class AutomaticDeletion extends StatefulWidget {
 
 class _AutomaticDeletion extends State<AutomaticDeletion> {
   bool warn = false;
+  int days;
 
   Widget build(BuildContext context) {
-    return Slider(
-      value: _currentSliderValue,
-      min: 10,
-      max: 25,
-      divisions: 15,
-      label: _currentSliderValue.round().toString(),
-      onChanged: (double value) {
-        setState(() {
-          //_currentSliderValue = value;
-          if (value < 14 && !warn) {
-            showDialog(
-              context: context,
-              builder: (_) => CupertinoAlertDialog(
-                content: Text(
-                    'Automatisches löschen unter 14 Tagen kann Probleme verursachen, sind Sie sich sicher?'),
-                actions: [
-                  CupertinoDialogAction(
-                      child: Text('Ja'),
-                      onPressed: () {
-                        warn = true;
-                        setState(() {});
-                        Navigator.pop(context);
-                      }),
-                  CupertinoDialogAction(
-                      child: Text('Nein'),
-                      onPressed: () {
-                        _currentSliderValue = 14;
-                        widget.settings.daysUntilRemoval =
-                            _currentSliderValue.toInt();
-                        saveSettings(widget.settings);
-                        setState(() {});
-                        Navigator.pop(context);
-                      }),
-                ],
-              ),
-            );
-            widget.settings.daysUntilRemoval = _currentSliderValue.toInt();
-            saveSettings(widget.settings);
-          }
-          if (value >= 14 || warn) {
-            _currentSliderValue = value;
-            widget.settings.daysUntilRemoval = _currentSliderValue.toInt();
-            saveSettings(widget.settings);
-            setState(() => {SettingsPage});
-          }
-        });
-      },
+    TextEditingController _controller = TextEditingController(
+        text: widget.settings.daysUntilRemoval.toString());
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+            width: 25.0,
+            child: TextFormField(
+              onFieldSubmitted: (text) {
+                days = int.parse(_controller.text);
+                if (days >= 10 && days < 100) {
+                  widget.settings.daysUntilRemoval =
+                      int.parse(_controller.text);
+                  saveSettings(widget.settings);
+                }
+                if (days < 10 || days > 100) {
+                  showDialog(
+                    context: context,
+                    builder: (_) => CupertinoAlertDialog(
+                      content: Text('Probleme und so blabla?'),
+                      actions: [
+                        CupertinoDialogAction(
+                            child: Text('Ok'),
+                            onPressed: () {
+                              setState(() {});
+                              Navigator.pop(context);
+                            }),
+                      ],
+                    ),
+                  );
+                }
+              },
+              //textAlign: TextAlign.center,
+              controller: _controller,
+              keyboardType: TextInputType.numberWithOptions(
+                  decimal: false, signed: false),
+              inputFormatters: <TextInputFormatter>[
+                WhitelistingTextInputFormatter.digitsOnly
+              ],
+            )),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            MaterialButton(
+              minWidth: 5.0,
+              child: Icon(Icons.arrow_drop_up),
+              onPressed: () {
+                int currentValue = int.parse(_controller.text);
+                setState(() {
+                  currentValue++;
+                  _controller.text =
+                      (currentValue).toString(); // incrementing value
+                });
+                days = int.parse(_controller.text);
+                if (days >= 10 && days < 100) {
+                  widget.settings.daysUntilRemoval =
+                      int.parse(_controller.text);
+                  saveSettings(widget.settings);
+                }
+                if (days < 10 || days >= 100) {
+                  showDialog(
+                    context: context,
+                    builder: (_) => CupertinoAlertDialog(
+                      content: Text('Probleme und so blabla?'),
+                      actions: [
+                        CupertinoDialogAction(
+                            child: Text('Ok'),
+                            onPressed: () {
+                              setState(() {});
+                              Navigator.pop(context);
+                            }),
+                      ],
+                    ),
+                  );
+                }
+              },
+            ),
+            MaterialButton(
+              minWidth: 5.0,
+              child: Icon(Icons.arrow_drop_down),
+              onPressed: () {
+                int currentValue = int.parse(_controller.text);
+                setState(() {
+                  print("Setting state");
+                  currentValue--;
+                  _controller.text =
+                      (currentValue).toString(); // decrementing value
+                });
+                days = int.parse(_controller.text);
+                if (days >= 10 && days < 100) {
+                  widget.settings.daysUntilRemoval =
+                      int.parse(_controller.text);
+                  saveSettings(widget.settings);
+                }
+                if (days < 10 || days >= 100) {
+                  showDialog(
+                    context: context,
+                    builder: (_) => CupertinoAlertDialog(
+                      content: Text('Probleme und so blabla?'),
+                      actions: [
+                        CupertinoDialogAction(
+                            child: Text('Ok'),
+                            onPressed: () {
+                              setState(() {});
+                              Navigator.pop(context);
+                            }),
+                      ],
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
